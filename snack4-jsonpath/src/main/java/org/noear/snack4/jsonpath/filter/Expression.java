d2b086a0ef1ca4b4e01f8bd7d128c10bb2d63cce
noear <noear@live.cn>
2025-10-11 18:27:48 +0800

    v4 优化代码

--- snack4-jsonpath/src/main/java/org/noear/snack4/jsonpath/filter/Expression.java
+++ snack4-jsonpath/src/main/java/org/noear/snack4/jsonpath/filter/Expression.java
@@ -206,5 +206,13 @@ public class Expression {
             this.type = type;
             this.value = value;
         }
+
+        @Override
+        public String toString() {
+            return "Token{" +
+                    "type=" + type +
+                    ", value='" + value + '\'' +
+                    '}';
+        }
     }
 }
