5d9484967967972d1ea1ce56582ca08f530c8ab3
noear <noear@live.cn>
2025-10-13 14:31:58 +0800

    v4 优化代码

---
+++ snack4-test/src/test/java/features/snack4/path/WarnTest.java
@@ -0,0 +1,31 @@
+package features.snack4.path;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.noear.snack4.jsonpath.JsonPathException;
+import org.noear.snack4.jsonpath.JsonPathProvider;
+import org.noear.snack4.jsonpath.JsonPathProviderWarn;
+
+/**
+ *
+ * @author noear 2025/10/13 created
+ *
+ */
+public class WarnTest {
+    @Test
+    public void case1(){
+        JsonPathProvider jsonPathProvider = new JsonPathProviderWarn();
+
+        Assertions.assertThrows(UnsupportedOperationException.class,() -> {
+            jsonPathProvider.select(null,null);
+        });
+
+        Assertions.assertThrows(UnsupportedOperationException.class,() -> {
+            jsonPathProvider.create(null,null);
+        });
+
+        Assertions.assertThrows(UnsupportedOperationException.class,() -> {
+            jsonPathProvider.delete(null,null);
+        });
+    }
+}
