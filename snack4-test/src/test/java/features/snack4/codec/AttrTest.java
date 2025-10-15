1bfb7b905a1717feb6b4efdca728f66e7f4f8dcf
noear <noear@live.cn>
2025-10-15 20:01:12 +0800

    v4 优化代码

--- snack4-test/src/test/java/features/snack4/codec/AttrTest.java
+++ snack4-test/src/test/java/features/snack4/codec/AttrTest.java
@@ -3,9 +3,13 @@ package features.snack4.codec;
 import lombok.Getter;
 import lombok.Setter;
 import org.junit.jupiter.api.Test;
+import org.noear.snack4.Feature;
 import org.noear.snack4.ONode;
 import org.noear.snack4.Options;
 import org.noear.snack4.annotation.ONodeAttr;
+import org.noear.snack4.codec.EncodeContext;
+import org.noear.snack4.codec.ObjectEncoder;
+import org.noear.snack4.codec.util.DateUtil;
 
 import java.util.Date;
 
@@ -17,30 +21,123 @@ import java.util.Date;
 public class AttrTest {
     @Test
     public void case1() {
-        Options options = Options.of().addEncoder(Date.class, (ctx, value, target) -> {
-//            if (ctx.getAttr() != null) {
-//                if (Asserts.isNotEmpty(ctx.getAttr().getFormat())) {
-//                    return target.setValue(ctx.getAttr().formatDate(value));
-//                }
-//            }
+        Options options = Options.of()
+                .addEncoder(Date.class, (ctx, value, target) -> {
+                    return target.setValue(value.getTime());
+                });
 
-            return target.setValue(value.getTime());
-        });
-
-        CustomDateDo dateDo = new CustomDateDo();
+        DemoDo1 dateDo = new DemoDo1();
 
         String json = ONode.ofBean(dateDo, options).toJson();
         System.out.println(json);
         assert "{\"date\":1760453997855,\"date2\":\"2025-10-14\"}".equals(json);
     }
 
+    @Test
+    public void case1_2() {
+        Options options = Options.of(Feature.Read_OnlyUseGetter)
+                .addEncoder(Date.class, (ctx, value, target) -> {
+                    return target.setValue(value.getTime());
+                });
+
+        DemoDo1 dateDo = new DemoDo1();
+
+        String json = ONode.ofBean(dateDo, options).toJson();
+        System.out.println(json);
+        assert "{\"date\":1760453997855,\"date2\":\"2025-10-14\"}".equals(json);
+    }
+
+    @Test
+    public void case2() {
+        Options options = Options.of(Feature.Read_AllowUseGetter)
+                .addEncoder(Date.class, (ctx, value, target) -> {
+                    return target.setValue(value.getTime());
+                });
+
+        DemoDo2 dateDo = new DemoDo2();
+
+        String json = ONode.ofBean(dateDo, options).toJson();
+        System.out.println(json);
+        assert "{\"date\":1760453997855,\"date2\":\"2025-10-14\"}".equals(json);
+    }
+
+    @Test
+    public void case2_2() {
+        Options options = Options.of()
+                .addEncoder(Date.class, (ctx, value, target) -> {
+                    return target.setValue(value.getTime());
+                });
+
+        DemoDo2 dateDo = new DemoDo2();
+
+        String json = ONode.ofBean(dateDo, options).toJson();
+        System.out.println(json);
+        assert "{\"date\":1760453997855,\"date2\":1760453997855}".equals(json);
+    }
+
+    @Test
+    public void case3() {
+        Options options = Options.of(Feature.Read_AllowUseGetter)
+                .addEncoder(Date.class, (ctx, value, target) -> {
+                    return target.setValue(value.getTime());
+                });
+
+        DemoDo3 dateDo = new DemoDo3();
+
+        String json = ONode.ofBean(dateDo, options).toJson();
+        System.out.println(json);
+        assert "{\"date\":1760453997855,\"date2\":\"2025-10-14\"}".equals(json);
+    }
+
+    @Test
+    public void case3_2() {
+        Options options = Options.of()
+                .addEncoder(Date.class, (ctx, value, target) -> {
+                    return target.setValue(value.getTime());
+                });
+
+        DemoDo3 dateDo = new DemoDo3();
+
+        String json = ONode.ofBean(dateDo, options).toJson();
+        System.out.println(json);
+        assert "{\"date\":1760453997855,\"date2\":1760453997855}".equals(json);
+    }
+
 
     @Setter
     @Getter
-    public static class CustomDateDo {
+    public static class DemoDo1 {
         private Date date = new Date(1760453997855L);
 
         @ONodeAttr(format = "yyyy-MM-dd")
         private Date date2 = new Date(1760453997855L);
+    }
+
+    public static class DemoDo2 {
+        private Date date = new Date(1760453997855L);
+        private Date date2 = new Date(1760453997855L);
+
+        @ONodeAttr(format = "yyyy-MM-dd")
+        public Date getDate2() {
+            return date2;
+        }
+    }
+
+    public static class DemoDo3 {
+        private Date date = new Date(1760453997855L);
+        private Date date2 = new Date(1760453997855L);
+
+        @ONodeAttr(encoder = DateEncoder.class)
+        public Date getDate2() {
+            return date2;
+        }
+    }
+
+    public static class DateEncoder implements ObjectEncoder<Date> {
+
+        @Override
+        public ONode encode(EncodeContext ctx, Date value, ONode target) {
+            return target.setValue(DateUtil.format(value, "yyyy-MM-dd"));
+        }
     }
 }
