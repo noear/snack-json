8ce3ec3e4d13037867cd33257f2c901051ac39d6
noear <noear@live.cn>
2025-10-26 18:54:52 +0800

    优化 与 snack3 的效果兼容性

--- snack4-test/src/test/java/features/snack4/codec/PropertiesTest.java
+++ snack4-test/src/test/java/features/snack4/codec/PropertiesTest.java
@@ -30,6 +30,20 @@ public class PropertiesTest {
     }
 
     @Test
+    public void case1_2() {
+        Properties properties = new Properties();
+        properties.put("aaa[1]", "2");
+        properties.put("aaa[0]", "1");
+        properties.put("id", "3");
+
+        UserModel rst = ONode.ofBean(properties, Feature.Write_AllowUseSetter).toBean(UserModel.class);
+        String json = ONode.ofBean(rst, Feature.Node_ToStringUseJson).toString();
+        System.out.println(json);
+
+        assert json.equals("{\"id\":3,\"aaa\":[1,2]}");
+    }
+
+    @Test
     public void case2() {
         KeyValueList properties = new KeyValueList();
         properties.add("aaa[]", "1");
